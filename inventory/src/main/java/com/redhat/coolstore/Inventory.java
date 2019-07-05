5c9128ac2b356c0e20c290e60e69f6ac6296ac1b
Daniel Oh <doh@danieloh.redhat.com>
2019-07-05 15:27:47 +0900

    inject sidecar to catalog, inventory

---
+++ inventory/src/main/java/com/redhat/coolstore/Inventory.java
@@ -0,0 +1,37 @@
+package com.redhat.coolstore;
+
+import javax.persistence.Cacheable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+@Entity
+@Cacheable
+public class Inventory extends PanacheEntity {
+
+    @Column
+    public String itemId;
+
+    @Column
+    public String location;
+
+	@Column
+    public int quantity;
+
+	@Column
+    public String link;
+
+    public Inventory() {
+
+    }
+
+    public Inventory(String itemId, int quantity, String location, String link) {
+        super();
+        this.itemId = itemId;
+        this.quantity = quantity;
+        this.location = location;
+        this.link = link;
+    }
+
+}
