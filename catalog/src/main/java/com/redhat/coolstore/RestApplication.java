ee60685b893d14df4d15473887d5971f89e74599
sshaaf <sshaaf@redhat.com>
2020-03-02 13:10:04 +0100

    fix Class names and pom for hoverfly dependency

--- catalog/src/main/java/com/redhat/coolstore/RestApplication.java
+++ catalog/src/main/java/com/redhat/coolstore/RestApplication.java
@@ -2,7 +2,8 @@ package com.redhat.coolstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
 @EnableFeignClients
