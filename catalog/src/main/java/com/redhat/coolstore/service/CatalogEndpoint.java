14d4af3255b6324fbb6c36f9b6b2964a8e1d77a0
danieloh30 <doh@redhat.com>
2020-03-02 23:54:26 -0500

    fix istio ingress issue

--- catalog/src/main/java/com/redhat/coolstore/service/CatalogEndpoint.java
+++ catalog/src/main/java/com/redhat/coolstore/service/CatalogEndpoint.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/api")
+@RequestMapping("/services")
 public class CatalogEndpoint {
 
     @Autowired
