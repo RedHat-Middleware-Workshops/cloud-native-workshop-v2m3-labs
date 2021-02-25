07a20f17f5eed5a0a41aba3a293d80b821197301
danieloh30 <doh@redhat.com>
2021-02-24 20:10:28 -0500

    Update M3 for OCP 4.7

--- istio/scripts/deploy-inventory.sh
+++ istio/scripts/deploy-inventory.sh
@@ -18,7 +18,7 @@ oc delete dc,deployment,bc,build,svc,route,pod,is --all
 echo "Waiting 30 seconds to finialize deletion of resources..."
 sleep 30
 
-oc new-app -e POSTGRESQL_USER=inventory \
+oc new-app --as-deployment-config -e POSTGRESQL_USER=inventory \
   -e POSTGRESQL_PASSWORD=mysecretpassword \
   -e POSTGRESQL_DATABASE=inventory openshift/postgresql:latest \
   --name=inventory-database
