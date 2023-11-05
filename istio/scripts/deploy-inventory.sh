71e3ba348e6fabae11d1ea1c27f1bfcabc0e035f
Daniel Oh <doh@redhat.com>
2023-11-05 13:58:18 -0500

    Update OCP 4.14 and DS 3.9

--- istio/scripts/deploy-inventory.sh
+++ istio/scripts/deploy-inventory.sh
@@ -18,17 +18,17 @@ oc delete dc,deployment,bc,build,svc,route,pod,is --all
 echo "Waiting 30 seconds to finialize deletion of resources..."
 sleep 30
 
-oc new-app --as-deployment-config -e POSTGRESQL_USER=inventory \
+oc new-app -e POSTGRESQL_USER=inventory \
   -e POSTGRESQL_PASSWORD=mysecretpassword \
-  -e POSTGRESQL_DATABASE=inventory openshift/postgresql:10-el8 \
+  -e POSTGRESQL_DATABASE=inventory registry.redhat.io/rhel9/postgresql-15 \
   --name=inventory-database
 
 mvn clean package -DskipTests -f $PROJECT_SOURCE/inventory
 
 oc delete route inventory
 
-oc label dc/inventory-database app.openshift.io/runtime=postgresql --overwrite && \
-oc label dc/inventory app.kubernetes.io/part-of=inventory --overwrite && \
-oc label dc/inventory-database app.kubernetes.io/part-of=inventory --overwrite && \
-oc annotate dc/inventory app.openshift.io/connects-to=inventory-database --overwrite && \
-oc annotate dc/inventory app.openshift.io/vcs-ref=ocp-4.14 --overwrite
+oc label deployment/inventory-database app.openshift.io/runtime=postgresql --overwrite && \
+oc label deployment/inventory app.kubernetes.io/part-of=inventory --overwrite && \
+oc label deployment/inventory-database app.kubernetes.io/part-of=inventory --overwrite && \
+oc annotate deployment/inventory app.openshift.io/connects-to=inventory-database --overwrite && \
+oc annotate deployment/inventory app.openshift.io/vcs-ref=ocp-4.14 --overwrite
